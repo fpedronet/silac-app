f9987d5b0848c83031abe5892c5891f1b6e76176
fpedronet <36646447+fpedronet@users.noreply.github.com>
2022-07-05 09:29:40 -0500

    merge

--- src/app/page/page-routing.module.ts
+++ src/app/page/page-routing.module.ts
@@ -5,17 +5,11 @@ import { GuardService } from '../_service/guard.service';
 import { HomeComponent } from './home/home.component';
 
 import { Not403Component } from './configuracion/not403/not403.component';
-import { LrendicionComponent } from './Administracion/ReportesAdministrativos/RendicionDeGastos/lrendicion/lrendicion.component';
-import { CrendicionComponent } from './Administracion/ReportesAdministrativos/RendicionDeGastos/crendicion/crendicion.component';
 
 const routes: Routes = [
   {path:'home', component: HomeComponent},
 
   {path: 'not-403', component: Not403Component},
-
-  {path:'administracion/rendicion/:idPantalla', component: LrendicionComponent, canActivate: [GuardService]},
-  {path:'administracion/rendicion/:idPantalla/create', component: CrendicionComponent, canActivate: [GuardService]},
-  {path:'administracion/rendicion/:idPantalla/edit/:id', component: CrendicionComponent, canActivate: [GuardService]},
 
   /********* Como ejemplo *******/
   // {path:'donante/donacion', component: LdonacionComponent, canActivate: [GuardService]},
--- src/app/page/page.module.ts
+++ src/app/page/page.module.ts
@@ -11,11 +11,7 @@ import { InterceptorService } from '../_interceptors/interceptor.service';
 import { Not404Component } from './configuracion/not404/not404.component';
 import { Not403Component } from './configuracion/not403/not403.component';
 
-import { LrendicionComponent } from './Administracion/ReportesAdministrativos/RendicionDeGastos/lrendicion/lrendicion.component';
-import { CrendicionComponent } from './Administracion/ReportesAdministrativos/RendicionDeGastos/crendicion/crendicion.component';
-import { FrendicionComponent } from './Administracion/ReportesAdministrativos/RendicionDeGastos/frendicion/frendicion.component';
-import { CdetalleComponent } from './Administracion/ReportesAdministrativos/RendicionDeGastos/cdetalle/cdetalle.component';
-import { CrechazoComponent } from './Administracion/ReportesAdministrativos/RendicionDeGastos/crechazo/crechazo.component';
+
 import { PerfilComponent } from './component/perfil/perfil.component';
 
 
@@ -26,11 +22,6 @@ import { PerfilComponent } from './component/perfil/perfil.component';
     HomeComponent,
     Not404Component,
     Not403Component,
-    LrendicionComponent,
-    CrendicionComponent,
-    FrendicionComponent,
-    CdetalleComponent,
-    CrechazoComponent,
     PerfilComponent
   ],
   imports: [
