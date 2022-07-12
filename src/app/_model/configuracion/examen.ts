9ba10a88cea32865f49b51574dd60e93b87ba8ca
Glenn Lozano <glozano@sistemas.com>
2022-07-12 16:37:40 -0500

    examenes

---
+++ src/app/_model/configuracion/examen.ts
@@ -0,0 +1,17 @@
+export class Examen{
+    nIdExamen?: number;
+    vCodExamen?: string;
+    vDescripcion?: string;
+    vAbreviatura?: string;
+    vCodExterno?: string;
+    nOrden?: number;
+    vUndMed?: string;
+    vCodTipoMuestra?: string;
+    vRangoRef?: string;
+    vCodAreaExamen?: string;
+    vFormula?: string;
+    vFormato?: string;
+    vTipoRespuesta?: string;
+    vDetalles?: string;
+    nIncDetalle?: number;
+}
--- src/app/page/configuracion/lexamen/lexamen.component.ts
+++ src/app/page/configuracion/lexamen/lexamen.component.ts
@@ -1,4 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from 'express';
+import { Examen } from 'src/app/_model/configuracion/examen';
+import { ConfigPermisoService } from 'src/app/_service/configpermiso.service';
+import { UsuarioService } from 'src/app/_service/configuracion/usuario.service';
+import { ConfimService } from '../../component/confirm/confim.service';
+import { SpinnerService } from '../../component/spinner/spinner.service';
+import { CexamenComponent } from '../cexamen/cexamen.component';
+import forms from 'src/assets/json/formulario.json';
+import { Permiso } from 'src/app/_model/permiso';
 
 @Component({
   selector: 'app-lexamen',
@@ -7,9 +18,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LexamenComponent implements OnInit {
 
-  constructor() { }
+  permiso: Permiso = {};
+  
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private dialog: MatDialog,
+    private spinner: SpinnerService,
+    private usuarioService : UsuarioService,
+    private configPermisoService : ConfigPermisoService,
+    private confirmService : ConfimService,
+  ) { }
 
   ngOnInit(): void {
+    this.obtenerpermiso();
+  }
+
+  obtenerpermiso(){
+    this.spinner.showLoading();
+    this.configPermisoService.obtenerpermiso(forms.reporteAdmin.codigo).subscribe(data=>{
+      this.permiso = data;
+       this.spinner.hideLoading();
+    });   
+  }
+
+  abrirDetalle(examen?: Examen){
+
+    const dialogRef = this.dialog.open(CexamenComponent, {
+      maxWidth: '100vw',
+      maxHeight: '100vh',
+      width: '850px',
+      panelClass: 'full-screen-modal',
+      disableClose: true,
+      data: {
+        curExamen: examen
+      }
+    });
   }
 
 }
