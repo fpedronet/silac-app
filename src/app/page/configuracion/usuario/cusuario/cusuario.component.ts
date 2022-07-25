a85c227b06ded82fce42616bd952814d77398916
fpedronet <36646447+fpedronet@users.noreply.github.com>
2022-07-25 17:12:08 -0500

    merge

--- src/app/page/configuracion/usuario/cusuario/cusuario.component.ts
+++ src/app/page/configuracion/usuario/cusuario/cusuario.component.ts
@@ -50,7 +50,6 @@ export class CusuarioComponent implements OnInit {
   listaTipoGenero?: TbMaestra[] = [];
   listaEstadoCivil?: TbMaestra[] = [];
   listaPerfil: Perfil[] = [];
-  // listaPais: Ubigeo[] = [];
   listaPais: Ubigeo[] = jsonPais;
 
 
